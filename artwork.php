15d35b5aaf2586d07d635bc92633d52095b50514
Matthew Sanders <sanders.me@gmail.com>
2020-12-24 12:35:18 -0600

    Better OO design template

--- artwork.php
+++ artwork.php
@@ -4,41 +4,87 @@ class Artwork {
     private $id;
     private $dbInfo;
 
-    function __construct($id) {
+    public function __construct($id) {
 
     }
 
-    function getTitle() {
-        return 'Lebrun - Self-portrait in a Straw Hat';
+    public function getTitle() {
+        return $this->getArtist(false).' - '.$this->getName();
     }
 
-    function getInfoPage() {
+    public function getName() {
+        return 'Self-portrait in a Straw Hat';
+    }
+
+    public function getArtist($fullName=true) {
+        if ($fullName) {
+            return 'Louise Elisabeth Lebrun';
+        } else {
+            return 'Lebrun';
+        }
+    }
+
+    public function getDescription() {
+        return 'The painting appears, after cleaning, to be an autograph replica of a picture,'.
+            ' the original of which was painted in Brussels in 1782 in free imitation of Rubens’s ’Chapeau de Paille’,'.
+            ' which LeBrun had seen in Antwerp. It was exhibited in Paris in 1782 at the Salon de la Correspondance.'.
+            ' LeBrun’s original is recorded in a private collection in France.';
+    }
+
+    public function getListPrice() {
+        return 700;
+    }
+
+    public function getFacets() {
+        return array(
+            'Date'=>'1782',
+            'Medium'=>'Oil on canvas',
+            'Dimension'=>'98cm x 71cm',
+        );
+    }
+
+    public function getLocation() {
+        return 'National Gallery, London';
+    }
+
+    public function getGenres() {
+        return array('Realism','Rococo');
+    }
+
+    public function getSubjects() {
+        return array('People','Arts');
+    }
+
+    public function getRelatedArt() {
+        return array(
+            293=>'Still Life with Flowers in a Glass Vase',
+            183=>'Portrait of Alida Christina Assink',
+            820=>'Self-portrait',
+            374=>'William II, Prince of Orange, and his Bride, Mary Stuart',
+            849=>'Milkmaid',
+        );
+    }
+
+    public function getInfoPage() {
         $html = '<article class="artwork">'.
-                '<h2 class="art_title">Self-portrait in a Straw Hat</h2>'.
-                '<p class="artist">By <a href="#">Louise Elisabeth Lebrun</a></p>'.
+                '<h2 class="art_title">'.$this->getName().'</h2>'.
+                '<p class="artist">By <a href="#">'.$this->getArtist().'</a></p>'.
                 '<figure>'.
-                    '<img src="artwork/medium/13.jpg" alt="Self-portrait in a Straw Hat" title="Self-portrait in a Straw Hat" />'.
+                    '<img src="artwork/medium/13.jpg" alt="'.$this->getName().'" title="'.$this->getName().'" />'.
                     '<figcaption>'.
-                        '<p>The painting appears, after cleaning, to be an autograph replica of a picture,'.
-                        ' the original of which was painted in Brussels in 1782 in free imitation of Rubens’s ’Chapeau de Paille’,'.
-                        ' which LeBrun had seen in Antwerp. It was exhibited in Paris in 1782 at the Salon de la Correspondance.'.
-                        ' LeBrun’s original is recorded in a private collection in France.</p>'.
-                        '<p class="list_price">$700</p>'.
+                        '<p>'.$this->getDescription().'</p>'.
+                        '<p class="list_price">'.formatUSD($this->getListPrice()).'</p>'.
                         '<div class="actions">'.
                             '<a href="#">Add to Wish List</a>'.
                             '<a href="#">Add to Shopping Cart</a>'.
                         '</div>'.
                         '<table class="artwork_info">'.
                             '<caption>Product Details</caption>';
-        $info = array(
-            'Date'=>'1782',
-            'Medium'=>'Oil on canvas',
-            'Dimension'=>'98cm x 71cm',
-        );
+        $info = $this->getFacets();
         $linkable = array(
-            'Home'=>array('National Gallery, London'),
-            'Genres'=>array('Realism','Rococo',),
-            'Subjects'=>array('People','Arts',),
+            'Home'=>array($this->getLocation()),
+            'Genres'=>$this->getGenres(),
+            'Subjects'=>$this->getSubjects(),
         );
         foreach ($info as $k=>$v) {
             $html .= '<tr>'.
@@ -83,16 +129,6 @@ class Artwork {
         }
         $html .= '</article>';
         return $html;
-    }
-
-    private function getRelatedArt() {
-        return array(
-            293=>'Still Life with Flowers in a Glass Vase',
-            183=>'Portrait of Alida Christina Assink',
-            820=>'Self-portrait',
-            374=>'William II, Prince of Orange, and his Bride, Mary Stuart',
-            849=>'Milkmaid',
-        );
     }
 
 }
--- util.php
+++ util.php
@@ -4,4 +4,8 @@ function getDB() {
     /* TODO */
 }
 
+function formatUSD($v) {
+    return sprintf('$%d.%02d',intval($v),($v*100)%100);
+}
+
 ?>
