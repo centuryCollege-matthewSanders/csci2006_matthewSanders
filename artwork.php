c7610c5f72f8b20d73c96453a62b77558297f436
Matthew Sanders <sanders.me@gmail.com>
2020-12-24 12:00:42 -0600

    Fixed up some minor errors - added related artwork HTML

--- artwork.php
+++ artwork.php
@@ -18,7 +18,7 @@ class Artwork {
                 '<p class="artist">By <a href="#">Louise Elisabeth Lebrun</a></p>'.
                 '<figure>'.
                     '<img src="artwork/medium/13.jpg" alt="Self-portrait in a Straw Hat" title="Self-portrait in a Straw Hat" />'.
-                    '<figcaption">'.
+                    '<figcaption>'.
                         '<p>The painting appears, after cleaning, to be an autograph replica of a picture,'.
                         ' the original of which was painted in Brussels in 1782 in free imitation of Rubens’s ’Chapeau de Paille’,'.
                         ' which LeBrun had seen in Antwerp. It was exhibited in Paris in 1782 at the Salon de la Correspondance.'.
@@ -34,9 +34,9 @@ class Artwork {
             'Date'=>'1782',
             'Medium'=>'Oil on canvas',
             'Dimension'=>'98cm x 71cm',
-            'Home'=>'National Gallery, London',
         );
         $linkable = array(
+            'Home'=>array('National Gallery, London'),
             'Genres'=>array('Realism','Rococo',),
             'Subjects'=>array('People','Arts',),
         );
@@ -62,7 +62,37 @@ class Artwork {
                     '</figcaption>'.
                 '</figure>'.
             '</article>';
+
+        $related = $this->getRelatedArt();
+        $html .= '<h2>Similar Artwork</h2>'.
+            '<article class="related">';
+        foreach ($related as $k=>$v) {
+            $html .= '<div class="relatedArt">'.
+                    '<figure>'.
+                        '<img src="artwork/small/'.$k.'.jpg" alt="'.$v.'" title="'.$v.'"/>'.
+                        '<figcaption>'.
+                            '<p><a href="#'.$k.'">'.$v.'</a></p>'.
+                        '</figcaption>'.
+                    '</figure>'.
+                    '<div class="actions">'.
+                        '<a href="#">View</a>'.
+                        '<a href="#">Wish</a>'.
+                        '<a href="#">Cart</a>'.
+                    '</div>'.
+                '</div>';
+        }
+        $html .= '</article>';
         return $html;
+    }
+
+    private function getRelatedArt() {
+        return array(
+            293=>'Still Life with Flowers in a Glass Vase',
+            183=>'Portrait of Alida Christina Assink',
+            820=>'Self-portrait',
+            374=>'William II, Prince of Orange, and his Bride, Mary Stuart',
+            849=>'Milkmaid',
+        );
     }
 
 }
